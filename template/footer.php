1f305413db1b0e01add8722cd9fc986407ffe47d
rebelucho <rebelucho@gmail.com>
2022-09-30 10:47:22 +0900

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,4 @@
+    <script src="/js/functions.js"></script>
+  </body>
+</html>
+
---
+++ template/header.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Darts28.ru: Score System</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+    <link rel="manifest" href="/img/site.webmanifest">
+
+  <!-- Bootstrap CSS -->
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!-- Bootstrap icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+  <!-- Bootstrap JS + Popper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  
+
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+  <!-- JS -->
+    <script src="/assets/js/NoSleep.min.js"></script> 
+    <script src="/js/functions.js"></script>
+  
+  </head>
+  <body>
+<header class="header">
+      <div class="container header-container">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/"><img src="/img/apple-touch-icon.png" width="40px" height="40px"> Online Scores</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link" href="/">Текущие игры</a>
+        <a class="nav-link" href="/howto.php">Как добавить устройство</a>
+        <a class="nav-link" href="https://darts28.ru" target="_blank">darts28.ru</a>
+      </div>
+    </div>
+  </div>
+</nav>
+</div>
+</header>
