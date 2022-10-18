d23cf5c3e3ecee6f95c787a798f2bdab94037744
rebelucho <rebelucho@gmail.com>
2022-10-18 20:12:30 +0900

    Update base.sql

--- inc/base.sql
+++ inc/base.sql
@@ -10,12 +10,16 @@ CREATE TABLE `games` (
   `id` int NOT NULL AUTO_INCREMENT,
   `guid` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `gamer1_name` varchar(120) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `sets1` int DEFAULT NULL,
   `legs1` int DEFAULT NULL,
   `gamer2_name` varchar(120) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `sets2` int DEFAULT NULL,
   `legs2` int DEFAULT NULL,
   `json` json DEFAULT NULL,
   `last_update` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `tag` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `end_match` binary(1) DEFAULT NULL,
+  `tag` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `code_version` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `guid` (`guid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
