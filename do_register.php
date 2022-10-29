b41eb154f950c5f1f2a29e55eaecc2802668a8fc
rebelucho <rebelucho@gmail.com>
2022-10-29 17:05:03 +0900

    passed change

--- do_register.php
+++ do_register.php
@@ -7,7 +7,7 @@ $stmt = pdo()->prepare("SELECT * FROM `users` WHERE `username` = :username");
 $stmt->execute(['username' => $_POST['username']]);
 if ($stmt->rowCount() > 0) {
     flash('Это имя пользователя уже занято.');
-    header('Location: /index1.php'); // Возврат на форму регистрации
+    header('Location: /register.php'); // Возврат на форму регистрации
     die; // Остановка выполнения скрипта
 }
 
