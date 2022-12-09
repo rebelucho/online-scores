e90d8ae3cdc281621835a941d9a42cd43e8dcf53
rebelucho <rebelucho@gmail.com>
2022-12-09 22:45:03 +0900

    mysql addapt

--- inc/base.sql
+++ inc/base.sql
@@ -7,10 +7,6 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
-DROP DATABASE IF EXISTS `games_darts28`;
-CREATE DATABASE `games_darts28` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `games_darts28`;
-
 DROP TABLE IF EXISTS `articles`;
 CREATE TABLE `articles` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -45,6 +41,42 @@ CREATE TABLE `games` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
+DROP TABLE IF EXISTS `p2p_games`;
+CREATE TABLE `p2p_games` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `guid_gamer1` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `gamer1_name` varchar(120) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `guid_gamer2` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `gamer2_name` varchar(120) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `current_throw` int DEFAULT NULL,
+  `require1` int DEFAULT NULL,
+  `score1` int DEFAULT NULL,
+  `darts1` int DEFAULT NULL,
+  `doubleAttempts1` int DEFAULT NULL,
+  `require2` int DEFAULT NULL,
+  `score2` int DEFAULT NULL,
+  `darts2` int DEFAULT NULL,
+  `doubleAttempts2` int DEFAULT NULL,
+  `sets1` int DEFAULT NULL,
+  `sets2` int DEFAULT NULL,
+  `legs1` int DEFAULT NULL,
+  `legs2` int DEFAULT NULL,
+  `player1` json DEFAULT NULL,
+  `player2` json DEFAULT NULL,
+  `setGame` binary(1) DEFAULT '0',
+  `deleteGame` binary(1) DEFAULT '0',
+  `privateStartGame` binary(1) DEFAULT '1',
+  `end_match` binary(1) DEFAULT NULL,
+  `remove` binary(1) DEFAULT NULL,
+  `gameData` json DEFAULT NULL,
+  `stat` json DEFAULT NULL,
+  `key` varchar(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `last_update` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `game_type` varchar(10) COLLATE utf8mb4_general_ci DEFAULT 'x01',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
 DROP TABLE IF EXISTS `roles`;
 CREATE TABLE `roles` (
   `role` int NOT NULL,
@@ -67,4 +99,4 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
--- 2022-11-28 02:25:07
+-- 2022-12-09 13:44:22
